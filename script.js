a823c54ecec06333ea53953786d24deab4a3c1f4
index-oo7 <lukaradovanovic01@gmail.com>
2023-05-18 01:22:39 +0200

    Added classes

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
 class knjiga {
     constructor(naziv, autor, brojStrana, godinaIzdavanja, kategorija ){
-        
+        this.id;
+        this.status;
+        this.naziv = naziv;
+        this.autor = autor;
+        this.brojStrana = brojStrana;
+        this.godinaIzdavanja = godinaIzdavanja;
+        this.kategorija = kategorija;
     }
 }
+
+class autor{
+    constructor(ime, prezime){
+        this.id;
+        this.ime = ime;
+        this.prezime = prezime;
+    }
+}
+
+class korisnik{
+    constructor(ime, prezime, lozinka, uloga){
+        this.id;
+        this.ime = ime;
+        this.prezime = prezime;
+        this.lozinka = lozinka;
+        this.uloga = uloga;
+    }
+}
+
+class Rezervacija {
+    constructor(idKorisnik, pocetakRezervacije, krajRezervacije){
+        this.id;
+        this.korisnik = idKorisnik;
+        this.pocetakRezervacije = pocetakRezervacije;
+        this.krajRezervacije = krajRezervacije;
+    }
+}
+
+
+//geteri, seteri, prebacivanje u php
