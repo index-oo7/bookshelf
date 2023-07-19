0bb1af75219c004476d937e9a9af21a84665ca03
index-oo7 <lukaradovanovic01@gmail.com>
2023-07-19 16:32:21 +0200

    Remove scriptAdmin.js connection and div acting as backdrop.

--- indexAdmin.php
+++ indexAdmin.php
@@ -231,11 +231,6 @@
         </form>
 
       </div>
-    <!-- zatamljenje kada se otvara prozor -->
-      <div id="pozadina"></div>
-
-    <!-- konekcija JS fajla koji se bavi animacijama -->
-      <script src="scriptAdmin.js"></script>
 
     <!-- konekcija bootrstrap-ovog JS-a -->
       <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
